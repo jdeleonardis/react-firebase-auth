3d0e5af65fa1983df1b258be48f0da92a874beb0
jdeleonardis <jdeleonardis@hotmail.com>
2021-01-25 15:41:16 -0500

    initial

---
+++ src/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const config = {
+    apiKey: "AIzaSyAMTk5uxptVQcMYZnd5kiBMV6rToZJov78",
+    authDomain: "react-firebase-authentic-de3ac.firebaseapp.com",
+    projectId: "react-firebase-authentic-de3ac",
+    storageBucket: "react-firebase-authentic-de3ac.appspot.com",
+    messagingSenderId: "257808067322",
+    appId: "1:257808067322:web:b824ce22de9f9b4f573743"
+  };
